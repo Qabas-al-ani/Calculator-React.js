2ca99f2cb1e3fd06ac38b63fb41b6cdc1355ed0d
Qabas al ani <doctorqa@yahoo.com>
2021-10-15 11:46:29 -0400

    created the first structure of the calculater and the buttons and the digits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 
-import './App.css';
-
 function App() {
+
+const createDigits = () => {
+  const digits = []
+  
+  for (let i = 1; i < 10; i++) {
+    digits.push(
+      <button key={i}>{i}</button>
+    )
+  }
+  return digits;
+}
+
+
+
   return (
-    <div >
-    
+    <div className="App">
+
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
